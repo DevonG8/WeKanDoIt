0cf1be931816801a2a71a8f1c0e63cca099bc53f
Devon Gay <devongay132@gmail.com>
2026-06-01 14:22:08 -0400

    fixing react import

--- src/components/Settings/Delete-data.tsx
+++ src/components/Settings/Delete-data.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { supabase } from "@/lib/supabase";
 import { useAuth } from "@/lib/AuthContext";
