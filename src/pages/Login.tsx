a06ed7d0853577d84c208df81ff60840f758050a
Devon Gay <devongay132@gmail.com>
2026-06-01 14:24:15 -0400

    chanign login colro theme

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
                 <a
                     href="../dashboard"
                     className="flex items-center gap-2 self-center font-medium">
-                    <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                    <div className="flex size-6 items-center justify-center rounded-md bg-primary">
                         <GalleryVerticalEnd className="size-4" />
                     </div>
                     WKD Inc.
